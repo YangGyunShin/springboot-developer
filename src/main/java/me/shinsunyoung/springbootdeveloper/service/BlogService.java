fbd7c634546a2b89b9d85b64e36b691c9d5a8431
YangGyunShin <skshtlsdidrb@gmail.com>
2024-11-02 21:55:20 +0900

    Update BlogService.java

--- src/main/java/me/shinsunyoung/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/service/BlogService.java
@@ -3,10 +3,13 @@ package me.shinsunyoung.springbootdeveloper.service;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.springbootdeveloper.domain.Article;
 import me.shinsunyoung.springbootdeveloper.dto.AddArticleRequest;
+import me.shinsunyoung.springbootdeveloper.dto.UpdateArticleRequest;
 import me.shinsunyoung.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +32,13 @@ public class BlogService {
 
     public void delete(Long id) {
         blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(Long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
