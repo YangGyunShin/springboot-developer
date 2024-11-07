c4db00182f70cf8f53c412208a2ba87f14fd06f3
YangGyunShin <skshtlsdidrb@gmail.com>
2024-11-07 20:38:52 +0900

    Update BlogViewController.java

--- src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.shinsunyoung.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.Article;
 import me.shinsunyoung.springbootdeveloper.dto.ArticleListViewResponse;
+import me.shinsunyoung.springbootdeveloper.dto.ArticleViewResponse;
 import me.shinsunyoung.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,6 +22,15 @@ public class BlogViewController {
     public String getArticles(Model model) {
         List<ArticleListViewResponse> articles = blogService.findAll().stream().map(ArticleListViewResponse::new).toList();
         model.addAttribute("articles", articles);
+
         return "articleList";
+    }
+
+    @GetMapping("/article/{id}")
+    public String getArticle(@PathVariable("id") Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
